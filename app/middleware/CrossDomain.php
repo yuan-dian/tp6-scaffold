113eabe2f162ca13ad1aa52e558a1a3e63260ca0
原点 <467490186@qq.com>
2024-12-12 10:32:59 +0800

    feat：优化跨域请求

--- app/middleware/CrossDomain.php
+++ app/middleware/CrossDomain.php
@@ -28,24 +28,19 @@ class CrossDomain
      */
     public function handle(Request $request, \Closure $next): Response
     {
-        // OPTIONS 预请求直接输出
-        if (strtoupper($request->method()) == "OPTIONS") {
-            $header = [
-                'Access-Control-Allow-Origin'  => $request->header('origin', '*'),
-                'Access-Control-Max-Age'       => 1800,
-                'Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers' => $request->header(
-                    'Access-Control-Request-Headers',
-                    'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-CSRF-TOKEN, X-Requested-With'
-                ),
-            ];
-            return Response::create('', 'json', '204')->header($header);
-        }
-
-        // 实际请求只需要增加Access-Control-Allow-Origin头即可
         $header = [
-            'Access-Control-Allow-Origin' => $request->header('origin', '*'),
+            'Access-Control-Allow-Origin'  => $request->header('origin', '*'),
+            'Access-Control-Allow-Headers' => $request->header(
+                'Access-Control-Request-Headers',
+                'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-CSRF-TOKEN, X-Requested-With'
+            ),
         ];
+        // OPTIONS 预请求直接输出
+        if ($request->method(true) == 'OPTIONS') {
+            $header['Access-Control-Max-Age'] = 1800;
+            $header['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS';
+            return Response::create()->code(204)->header($header);
+        }
         return $next($request)->header($header);
     }
 }
